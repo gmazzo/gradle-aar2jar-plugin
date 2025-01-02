f470e372989a70d819e8ee6b461d95353a04a24b
Guillermo Mazzola <gmazzo65@gmail.com>
2025-01-02 01:23:46 +0100

    Update Gradle Wrapper from 8.11.1 to 8.12 (#4)
    * Update Gradle Wrapper from 8.11.1 to 8.12
    
    Signed-off-by: gradle-update-robot <gradle-update-robot@regolo.cc>
    
    * Added Gradle issue workaround
    
    ---------
    
    Signed-off-by: gradle-update-robot <gradle-update-robot@regolo.cc>
    Co-authored-by: gradle-update-robot <gradle-update-robot@regolo.cc>

--- demo/src/testFixtures/kotlin/io/github/gmazzo/gradle/aar2jar/demo/BrowserHelper.kt
+++ demo/src/testFixtures/kotlin/io/github/gmazzo/gradle/aar2jar/demo/BrowserHelper.kt
@@ -1,7 +1,5 @@
 package io.github.gmazzo.gradle.aar2jar.demo
 
-import androidx.browser.browseractions.BrowserActionItem
-import androidx.browser.customtabs.CustomTabsClient
 import androidx.browser.customtabs.TrustedWebUtils
 
 object BrowserHelper {
