cbb9f60c5d7e57c711b478fd90ca051e2f0580d3
Guillermo Mazzola <gmazzo65@gmail.com>
2025-03-12 15:45:28 +0100

    Updated CI/CD process to publish to Maven Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    alias(libs.plugins.publicationsReport)
     base
     `maven-publish`
 }
--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -2,31 +2,67 @@ plugins {
     alias(libs.plugins.importClasses)
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.samReceiver)
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.axion.release)
+    alias(libs.plugins.mavenPublish)
     alias(libs.plugins.gradle.pluginPublish)
-    signing
+    alias(libs.plugins.publicationsReport)
     jacoco
 }
 
 group = "io.github.gmazzo.aar2jar"
-description = "AAR dependency support for Java Plugin"
-version = providers
-    .exec { commandLine("git", "describe", "--tags", "--always") }
-    .standardOutput.asText.get().trim().removePrefix("v")
+description = "Adds AAR dependency support for Java"
+version = scmVersion.version
 
 java.toolchain.languageVersion = JavaLanguageVersion.of(libs.versions.java.get())
 samWithReceiver.annotation(HasImplicitReceiver::class.qualifiedName!!)
 
+val originUrl = providers
+    .exec { commandLine("git", "remote", "get-url", "origin") }
+    .standardOutput.asText.map { it.trim() }
+
 gradlePlugin {
-    website.set("https://github.com/gmazzo/gradle-aar2jar-plugin")
-    vcsUrl.set("https://github.com/gmazzo/gradle-aar2jar-plugin")
+    website = originUrl
+    vcsUrl = originUrl
 
     plugins {
         create("publications-report") {
             id = "io.github.gmazzo.aar2jar"
             displayName = name
             implementationClass = "io.github.gmazzo.gradle.aar2jar.AAR2JARPlugin"
-            description = "Adds AAR dependency support for Java"
+            description = project.description
             tags.addAll("android", "aar", "jar", "java", "aar2jar", "aar-jar")
+        }
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral("CENTRAL_PORTAL", automaticRelease = true)
+
+    pom {
+        name = "${rootProject.name}-${project.name}"
+        description = provider { project.description }
+        url = originUrl
+
+        licenses {
+            license {
+                name = "MIT License"
+                url = "https://opensource.org/license/mit/"
+            }
+        }
+
+        developers {
+            developer {
+                id = "gmazzo"
+                name = id
+                email = "gmazzo65@gmail.com"
+            }
+        }
+
+        scm {
+            connection = originUrl
+            developerConnection = originUrl
+            url = originUrl
         }
     }
 }
@@ -58,19 +94,6 @@ dependencies {
     testRuntimeOnly(libs.junit.engine)
 }
 
-signing {
-    val signingKey: String? by project
-    val signingPassword: String? by project
-
-    useInMemoryPgpKeys(signingKey, signingPassword)
-    sign(publishing.publications)
-    isRequired = signingKey != null || providers.environmentVariable("GRADLE_PUBLISH_KEY").isPresent
-}
-
-tasks.publish {
-    dependsOn(tasks.publishPlugins)
-}
-
 tasks.test {
     useJUnitPlatform()
     finalizedBy(tasks.jacocoTestReport)
@@ -78,4 +101,22 @@ tasks.test {
 
 tasks.jacocoTestReport {
     reports.xml.required = true
+}
+
+afterEvaluate {
+    tasks.named<Jar>("javadocJar") {
+        from(tasks.dokkaGeneratePublicationJavadoc)
+    }
+}
+
+tasks.withType<PublishToMavenRepository>().configureEach {
+    mustRunAfter(tasks.publishPlugins)
+}
+
+tasks.publishPlugins {
+    enabled = "$version".matches("\\d+(\\.\\d+)+".toRegex())
+}
+
+tasks.publish {
+    dependsOn(tasks.publishPlugins)
 }
