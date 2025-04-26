7adcb5c274a46e85880fae02ace851132da3c46b
gradle-update-robot <gradle-update-robot@regolo.cc>
2025-04-26 00:57:32 +0000

    Update Gradle Wrapper from 8.13 to 8.14
    Signed-off-by: gradle-update-robot <gradle-update-robot@regolo.cc>

--- plugin/src/test/kotlin/io/github/gmazzo/gradle/aar2jar/AAR2JARPluginTest.kt
+++ plugin/src/test/kotlin/io/github/gmazzo/gradle/aar2jar/AAR2JARPluginTest.kt
@@ -1,18 +1,5 @@
 package io.github.gmazzo.gradle.aar2jar
 
-import org.gradle.api.Action
-import org.gradle.api.problems.Problem
-import org.gradle.api.problems.ProblemId
-import org.gradle.api.problems.ProblemSpec
-import org.gradle.api.problems.internal.AdditionalDataBuilderFactory
-import org.gradle.api.problems.internal.InternalProblem
-import org.gradle.api.problems.internal.InternalProblemBuilder
-import org.gradle.api.problems.internal.InternalProblemReporter
-import org.gradle.api.problems.internal.InternalProblemSpec
-import org.gradle.api.problems.internal.InternalProblems
-import org.gradle.api.problems.internal.ProblemsProgressEventEmitterHolder
-import org.gradle.internal.operations.OperationIdentifier
-import org.gradle.internal.reflect.Instantiator
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.get
 import org.gradle.testfixtures.ProjectBuilder
@@ -36,8 +23,6 @@ class AAR2JARPluginTest {
     )
     fun `can resolve AAR dependencies`(configuration: String, expectedArtifacts: String): Unit =
         with(ProjectBuilder.builder().build()) {
-            gradleIssue31862Workaround()
-
             apply(plugin = "java")
             apply(plugin = "io.github.gmazzo.aar2jar")
 
@@ -50,83 +35,5 @@ class AAR2JARPluginTest {
 
             assertEquals(expectedArtifacts.split('|').toSet(), resolvedArtifacts)
         }
-
-    // TODO workaround for https://github.com/gradle/gradle/issues/31862
-    private fun gradleIssue31862Workaround() = ProblemsProgressEventEmitterHolder.init(object : InternalProblems {
-
-        override fun getInternalReporter() = object : InternalProblemReporter {
-
-            override fun report(
-                problem: Problem,
-                id: OperationIdentifier
-            ) {
-                TODO("Not yet implemented")
-            }
-
-            override fun internalCreate(action: Action<in InternalProblemSpec>): InternalProblem {
-                TODO("Not yet implemented")
-            }
-
-            override fun create(
-                problemId: ProblemId,
-                action: Action<in ProblemSpec>
-            ): Problem {
-                TODO("Not yet implemented")
-            }
-
-            override fun report(
-                problemId: ProblemId,
-                spec: Action<in ProblemSpec>
-            ) {
-                TODO("Not yet implemented")
-            }
-
-            override fun report(problem: Problem) {
-                TODO("Not yet implemented")
-            }
-
-            override fun report(problems: Collection<Problem?>) {
-                TODO("Not yet implemented")
-            }
-
-            override fun throwing(
-                exception: Throwable,
-                problemId: ProblemId,
-                spec: Action<in ProblemSpec>
-            ): RuntimeException {
-                TODO("Not yet implemented")
-            }
-
-            override fun throwing(
-                exception: Throwable,
-                problem: Problem
-            ): RuntimeException {
-                TODO("Not yet implemented")
-            }
-
-            override fun throwing(
-                exception: Throwable,
-                problems: Collection<Problem?>
-            ): RuntimeException {
-                TODO("Not yet implemented")
-            }
-
-        }
-
-        override fun getAdditionalDataBuilderFactory(): AdditionalDataBuilderFactory {
-            TODO("Not yet implemented")
-        }
-
-        override fun getInstantiator(): Instantiator {
-            TODO("Not yet implemented")
-        }
-
-        override fun getProblemBuilder(): InternalProblemBuilder {
-            TODO("Not yet implemented")
-        }
-
-        override fun getReporter() = getInternalReporter()
-
-    })
 
 }
